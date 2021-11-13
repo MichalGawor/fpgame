92a33abea1a514ba375dbf2f189e5d695076a560
gawor <michal.j.gawor@gmail.com>
2021-11-13 20:22:12 +0100

    fast forward

--- game/app/Main.hs
+++ game/app/Main.hs
@@ -13,21 +13,35 @@ import Objects.Projectiles.Weapon
 import Objects.Ships
 import Plane
 
-noTarget :: Target Point
-noTarget = NoTarget
+
+
+-- main :: IO ()
+-- main = do 
+--         putStrLn ""
+--             do 
+--             movedShip <- move testSuicideShip (noTarget)
+--             putStrLn (show (movedShip))
+
+-- testSuicideShip :: SuicideShip
+-- testSuicideShip = MkSuicideShip testShip (Degrees (fromIntegral 5))
+
+-- testShip :: Ship
+-- testShip = Ship {maxHp=100, currHp=100, weapon=NoWeapon, position=(50, 50), velocity=(2, 0), collisionDamage=20}
+
+-- testGunShip :: GunShip 
+-- testGunShip = MkGunShip testShip
+
+-- noTarget :: Target Point
+-- noTarget = NoTarget
+
+import Graphics.Gloss.Interface.IO.Game
+
+
+import Controller
+import Model
+import View
+
+import UI_FiniteStateMachine
 
 main :: IO ()
-main = do 
-        putStrLn (show testSuicideShip)
-            do 
-            movedShip <- move testSuicideShip (noTarget)
-            putStrLn (show (movedShip))
-
-testSuicideShip :: SuicideShip
-testSuicideShip = MkSuicideShip testShip (Degrees (fromIntegral 5))
-
-testShip :: Ship
-testShip = Ship {maxHp=100, currHp=100, weapon=NoWeapon, position=(50, 50), velocity=(2, 0), collisionDamage=20}
-
-testGunShip :: GunShip 
-testGunShip = MkGunShip testShip
+main = let Appl
